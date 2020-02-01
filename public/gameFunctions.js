4872e4282b9170bb448d91f0889fb4530af74ae9
magiobus <aalbertobus@gmail.com>
2020-02-01 16:55:49 -0700

    NodeJS Basic Hello World Server

---
+++ public/gameFunctions.js
@@ -0,0 +1,10 @@
+
+
+var gameFunctions = {
+	loadGameImage: function(imageUrl){
+		console.log("loading image...");
+		img = loadImage(imageUrl);
+		image(img, 0, 0);
+	}
+}
+
---
+++ public/sketch.js
@@ -0,0 +1,12 @@
+function setup() { 
+  createCanvas(1920, 1080);
+    console.log("mandando llamar funcion")
+  	// gameFunctions.loadGameImage("rawPhotos/DSC06961.JPG");
+} 
+
+function draw() { 
+  background(0,255,255);
+  ellipse(50,50,50,50);
+}
+
+
